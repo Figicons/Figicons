0d4f2ca70fb4c3798869b9afe92ef25ae3b03426
Stefan Mansson <st.mansson@icloud.com>
2018-11-02 23:34:43 +0200

    Update Usage docs

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -17,12 +17,12 @@ const Usage = () => (
             </p>
         </div>
         <div className="mw7 center">
-            <section>
+            <section className="desc">
                 <h2>Setting up Figma</h2>
                 <p>It all starts from your</p>
             </section>
 
-            <section>
+            <section className="desc">
                 <h2>Fetching &amp; building your iconset</h2>
                 <p>
                     We make it effortless for you at this point to fetch, clean, optimize &amp; setup all your icons in a neat little file.
@@ -35,7 +35,7 @@ const Usage = () => (
                 </div>
             </section>
 
-            <section>
+            <section className="desc">
                 <h2>Setting up icon options</h2>
                 <p>
                     You can customize the look of your icons by editing the icon attributes file. By default, icons are 24x24 and have a 2px
@@ -43,7 +43,7 @@ const Usage = () => (
                 </p>
             </section>
 
-            <section>
+            <section className="desc">
                 <h2>Preparing for usage</h2>
                 <p>
                     It's time to bundle your custom icons into React &amp; Web Components (you can use whichever). Run the below command in
--- src/components/Usage.tsx
+++ src/components/Usage.tsx
@@ -13,26 +13,37 @@ const Usage = () => (
         </div>
 
         <div className="mw7 center">
-            <section>
-                <h2>Using Web Components</h2>
-                <p>
-                    Web Components are a simple and performant way to include Figicons on your site. Each icon is dynamically loaded into
-                    its component and comes with support for attribute overrides.
-                </p>
-                <h3>Markup</h3>
-                <div className="snippet">
-                    <small>Basic Usage</small>
-                    <Highlight className="html">{'<fig-icon name="heart"></fig-icon>'}</Highlight>
-                </div>
-            </section>
-
-            <section>
+            <section className="desc">
                 <h2>Using the React component</h2>
                 <p>
                     Web Components are a simple and performant way to include Figicons on your site. Each icon is dynamically loaded into
                     its component and comes with support for attribute overrides.
                 </p>
+
+                <h3>Installation</h3>
+                <p>Add Figicons to your project by either installing with NPM or Yarn:</p>
+                <div className="snippet">
+                    <small>NPM</small>
+                    <code className="white">
+                        <span className="gray">$</span> npm install figicons
+                    </code>
+                </div>
+                <div className="snippet">
+                    <small>Yarn</small>
+                    <code className="white">
+                        <span className="gray">$</span> yarn add figicons
+                    </code>
+                </div>
+
                 <h3>Markup</h3>
+                <p>
+                    Import Figicons, then use the basic component where the name attribute is required, or override certain attributes
+                    inline with the component as props:
+                </p>
+                <div className="snippet">
+                    <small>JS</small>
+                    <Highlight className="js">{"import Figicon from 'figicons/react';"}</Highlight>
+                </div>
                 <div className="snippet">
                     <small>Basic Usage</small>
                     <Highlight className="html">{'<Figicon name="heart" />'}</Highlight>
@@ -40,6 +51,40 @@ const Usage = () => (
                 <div className="snippet">
                     <small>Attribute overrides</small>
                     <Highlight className="html">{'<Figicon name="heart" stroke="red" strokeWidth={1} />'}</Highlight>
+                </div>
+            </section>
+
+            <section className="desc">
+                <h2>Using Web Components</h2>
+                <p>
+                    Web Components are a simple and performant way to include Figicons on your site. Each icon is dynamically loaded into
+                    its component and comes with support for attribute overrides.
+                </p>
+
+                <h3>Installation</h3>
+                <p>Add the following Script tag, linking to the Figicon Web Component CDN, to the Head of your markup:</p>
+                <div className="snippet">
+                    <small>
+                        HTML <span>cdn</span>
+                    </small>
+                    <Highlight className="html">{'<script src="https://cdn.com/figicons/webcomponent.min.js"></script>'}</Highlight>
+                </div>
+                <div className="snippet">
+                    <small>
+                        HTML <span>relative</span>
+                    </small>
+                    <Highlight className="html">{'<script src="/path/to/figicons/webcomponent.min.js"></script>'}</Highlight>
+                </div>
+
+                <h3>Markup</h3>
+                <p>Use the basic component where the name attribute is required, or override certain attributes:</p>
+                <div className="snippet">
+                    <small>Basic Usage</small>
+                    <Highlight className="html">{'<fig-icon name="heart"></fig-icon>'}</Highlight>
+                </div>
+                <div className="snippet">
+                    <small>Attribute overrides</small>
+                    <Highlight className="html">{'<fig-icon name="heart" size="16"></fig-icon>'}</Highlight>
                 </div>
             </section>
         </div>
