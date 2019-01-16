241e6a679c448b000e6b2194691eb95406351ae4
Stefan Mansson <stefan.aotik@gmail.com>
2019-01-16 21:56:43 +0200

    Store keystore, select saved keys

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,82 @@
 #!/usr/bin/env node --harmony
 
-/**
- * Module dependencies.
- */
-
 const program = require('commander');
-const co = require('co');
-const prompt = require('co-prompt');
+const inquirer = require('inquirer');
+const storage = require('node-persist');
 const fetch = require('../scripts/fetch');
 const package = require('../package.json');
+const keyStoreDir = './bin/store/keyStore';
 
-program
-    .name('figicons')
-    .version(package.version)
-    .option('-K, --key', 'Figma project key')
-    .option('-T, --token', 'Figma account token')
-    .action(function(env, options) {
-        co(function*() {
-            const key = yield prompt('Enter your Figma File key: ');
-            const token = yield prompt('Enter your Figma Personal Access Token: ');
-            console.log('key: %s token: %s', key, token);
+async function run() {
+    const keyStore = storage.create({ dir: keyStoreDir });
+    await keyStore.init();
 
-            fetch(key, token);
-        });
-    })
-    .parse(process.argv);
+    const allKeys = await keyStore.keys();
+
+    const keysList =
+        allKeys.length > 0
+            ? [...allKeys, new inquirer.Separator()]
+            : [{ name: 'No saved project found', disabled: 'Create a new one below' }, new inquirer.Separator()];
+
+    program
+        .name('figicons')
+        .version(package.version)
+        .option('-K, --key', 'Figma project key')
+        .option('-T, --token', 'Figma account token')
+        .action(function(env, options) {
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'selectedKey',
+                        message: 'Select a saved Figma project, or a create new one',
+                        choices: [...keysList, 'Create new'],
+                    },
+                    {
+                        type: 'input',
+                        name: 'key',
+                        message: 'Enter the file key of your Figma project',
+                        when: function(answers) {
+                            return answers.selectedKey === 'Create new';
+                        },
+                    },
+                    {
+                        type: 'input',
+                        name: 'token',
+                        message: 'Enter a personal access token (leave blank if project is public)',
+                        when: function(answers) {
+                            return answers.selectedKey === 'Create new';
+                        },
+                    },
+                ])
+                .then(async answers => {
+                    const { key, token, selectedKey } = answers;
+                    let config = { key, token };
+
+                    if (key && token) {
+                        await keyStore.setItem(key, token);
+                        config.key = key;
+                        config.token = token;
+                    } else {
+                        config.key = selectedKey;
+                        config.token = await keyStore.getItem(selectedKey);
+                    }
+
+                    console.log(config);
+                    fetch(config.key, config.token);
+
+                    console.log(JSON.stringify(answers, null, '  '));
+                });
+
+            // co(function*() {
+            //     const key = yield prompt('Enter your Figma File key: ');
+            //     const token = yield prompt('Enter your Figma Personal Access Token: ');
+            //     console.log('key: %s token: %s', key, token);
+
+            //     fetch(key, token);
+            // });
+        })
+        .parse(process.argv);
+}
+
+run();
