d8c226c027e36a55ae38662c6ef9076c41665209
Stefan Mansson <stefan.aotik@gmail.com>
2019-01-20 21:58:03 +0200

    Self invoke run

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,7 @@ const storage = require('node-persist');
 const package = require('../package.json');
 const keyStoreDir = './bin/store/keyStore';
 
-async function run() {
+(async function run() {
     const parser = new Parser();
     const messager = new Messager();
     const keyStore = storage.create({ dir: keyStoreDir });
@@ -38,76 +38,72 @@ async function run() {
         .version(package.version)
         .option('-K, --key', 'Figma project key')
         .option('-T, --token', 'Figma account token')
-        .action(function(env, options) {
-            inquirer
-                .prompt([
-                    {
-                        type: 'list',
-                        name: 'selectedKey',
-                        message: 'Select a saved Figma project, or a create new one',
-                        choices: [...keysList, new inquirer.Separator(), 'Create new'],
+        .action(async function(env, options) {
+            const { key, token, selectedKey } = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'selectedKey',
+                    message: 'Select a saved Figma project, or a create new one',
+                    choices: [...keysList, new inquirer.Separator(), 'Create new'],
+                },
+                {
+                    type: 'input',
+                    name: 'key',
+                    message: 'Enter the file key of your Figma project',
+                    when: function(answers) {
+                        return answers.selectedKey === 'Create new';
                     },
-                    {
-                        type: 'input',
-                        name: 'key',
-                        message: 'Enter the file key of your Figma project',
-                        when: function(answers) {
-                            return answers.selectedKey === 'Create new';
-                        },
+                },
+                {
+                    type: 'input',
+                    name: 'token',
+                    message: 'Enter a personal access token (leave blank if project is public)',
+                    when: function(answers) {
+                        return answers.selectedKey === 'Create new';
                     },
-                    {
-                        type: 'input',
-                        name: 'token',
-                        message: 'Enter a personal access token (leave blank if project is public)',
-                        when: function(answers) {
-                            return answers.selectedKey === 'Create new';
-                        },
-                    },
-                ])
-                .then(async answers => {
-                    const { key, token, selectedKey } = answers;
-                    let config = { key, token };
+                },
+            ]);
 
-                    if (key && token) {
-                        config.key = key;
-                        config.token = token;
-                    } else {
-                        const { token: selectedToken } = await keyStore.getItem(selectedKey);
-                        config.key = selectedKey;
-                        config.token = selectedToken;
-                    }
+            let config = { key, token };
 
-                    const fetcher = new Fetcher({
-                        key: config.key,
-                        token: config.token,
-                    });
+            if (key && token) {
+                config.key = key;
+                config.token = token;
+            } else {
+                const { token: selectedToken } = await keyStore.getItem(selectedKey);
+                config.key = selectedKey;
+                config.token = selectedToken;
+            }
 
-                    try {
-                        messager.startLoading('Fetching icons from Figma');
-                        const figmaData = await fetcher.request(`files/${config.key}`);
-                        await fetcher.grabImageData(figmaData.document.children[0].children);
-                        messager.endLoading(`Fetched ${figmaData.document.children[0].children.length} icons from: ${figmaData.name}`);
+            const fetcher = new Fetcher({
+                key: config.key,
+                token: config.token,
+            });
 
-                        messager.startLoading('Cleaning & optimizing icons');
-                        await parser.clean();
-                        messager.endLoading(`Cleaned & optimized icons`);
+            try {
+                const figmaData = await fetcher.request(`files/${config.key}`);
 
-                        messager.startLoading('Parsing icons');
-                        await parser.parse();
-                        messager.endLoading(`Parsed icons`);
+                messager.startLoading('Fetching icons from Figma');
+                await fetcher.grabImageData(figmaData.document.children[0].children);
+                messager.endLoading(`Fetched ${figmaData.document.children[0].children.length} icons from: ${figmaData.name}`);
 
-                        await keyStore.setItem(config.key, {
-                            name: figmaData.name,
-                            token: config.token,
-                        });
-                    } catch (error) {
-                        messager.log(error.message);
-                    }
+                messager.startLoading('Cleaning & optimizing icons');
+                await parser.clean();
+                messager.endLoading(`Cleaned & optimized icons`);
 
-                    // console.log(JSON.stringify(answers, null, '  '));
+                messager.startLoading('Parsing icons');
+                await parser.parse();
+                messager.endLoading(`Parsed icons`);
+
+                await keyStore.setItem(config.key, {
+                    name: figmaData.name,
+                    token: config.token,
                 });
+            } catch (error) {
+                messager.log(error.message);
+            }
+
+            // console.log(JSON.stringify(answers, null, '  '));
         })
         .parse(process.argv);
-}
-
-run();
+})();
