d0017233a32a1c80eb39a9d31c722edde56e57a7
Stefan Mansson <stefan.aotik@gmail.com>
2019-01-22 20:42:18 +0200

    Cleanup, embed rollup config

--- scripts/Packager.js
+++ scripts/Packager.js
@@ -1,52 +1,67 @@
 const path = require('path');
-const webpack = require('webpack');
-const config = require('../webpack.config');
-const FolderManager = require('./FolderManager');
 const Messager = require('./Messager');
+const rollup = require('rollup');
+const typescript = require('rollup-plugin-typescript2');
+const commonjs = require('rollup-plugin-commonjs');
+const external = require('rollup-plugin-peer-deps-external');
+const resolve = require('rollup-plugin-node-resolve');
+const json = require('rollup-plugin-json');
+const replace = require('rollup-plugin-replace');
 
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
+const output = [
+    {
+        file: 't.cjs.js',
+        format: 'cjs',
+        exports: 'named',
+    },
+    {
+        file: 't.es.js',
+        format: 'es',
+        exports: 'named',
+    },
+    {
+        file: 't.umd.js',
+        format: 'umd',
+        name: 'figicons',
+        exports: 'named',
+    },
+];
+const plugins = [
+    json({
+        compact: true,
+    }),
+    external(),
+    resolve(),
+    typescript({
+        clean: true,
+        rollupCommonJSResolveHack: true,
+        exclude: ['*.d.ts', '**/*.d.ts'],
+    }),
+    commonjs({
+        include: ['node_modules/**'],
+        exclude: ['node_modules/process-es6/**'],
+        namedExports: {
+            'node_modules/react/index.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+        },
+    }),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+];
 
 class Packager {
     static async package() {
-        return new Promise(resolve => {
-            Messager.startLoading(`🛠  Packaging Components`);
+        console.log('e');
+        const outputOptions = rollupConfig.output;
 
-            webpack(config, (err, stats) => {
-                if (err) {
-                    console.error(err.stack || err);
-                    if (err.details) {
-                        console.error(err.details);
-                    }
-                    return;
-                }
+        [path.join(__dirname, '..', 'components/FigiconReact.tsx')].forEach(async input => {
+            const bundle = await rollup.rollup({ input, plugins });
 
-                const info = stats.toJson();
-
-                if (stats.hasErrors()) {
-                    console.error(info.errors);
-                }
-
-                if (stats.hasWarnings()) {
-                    console.warn(info.warnings);
-                }
-                Messager.endLoading(`📦  %s Packaged React & Web Components`);
-                resolve();
+            output.forEach(o => {
+                bundle.write(o);
             });
         });
-    }
-
-    static compile() {
-        var tsProject = ts.createProject('tsconfig.json', { outFile: 'output.js' });
-
-        var tsResult = tsProject
-            .src() // or tsProject.src()
-            .pipe(tsProject());
-        console.log(tsResult.js);
-        return tsResult.js.pipe(gulp.dest('release'));
     }
 }
 
 module.exports = Packager;
-
-Packager.compile();
