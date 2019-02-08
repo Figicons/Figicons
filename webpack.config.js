40275f697daad1ec9b132450cdb6e75b3e801b3c
Stefan Mansson <stefan.aotik@gmail.com>
2019-02-08 19:46:46 +0200

    Create UMD outputs with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    WebComponent: path.join(__dirname, "./components/FigiconWebComponent.ts"),
+    ReactComponent: path.join(__dirname, "./components/FigiconReact.tsx")
+  },
+  devtool: "none",
+  output: {
+    library: "Figicons",
+    libraryTarget: "umd",
+    path: path.join(__dirname, "umd"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
