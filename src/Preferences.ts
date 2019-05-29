8ffa021be9766e5642cf2c3edfe905c81f0ba140
Stefan Mansson <stefan.aotik@gmail.com>
2019-05-29 21:59:21 +0300

    refactor: move iconAttrs into src

--- src/Preferences.ts
+++ src/Preferences.ts
@@ -1,5 +1,5 @@
 import * as defaultIcons from '../figicons/figicons.json';
-import * as iconAttrs from '../configs/iconAttributes.json';
+import * as iconAttrs from './iconAttributes.json';
 import { IConfig } from '../types/interfaces';
 
 export default class Preferences {
